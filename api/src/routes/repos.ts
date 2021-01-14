11097e8a7c1127ba38dc90ed11cd052b666315fd
deepmal933 <deepmal944@gmail.com>
2021-01-14 13:09:12 -0500

    created services for reading and fetching repos

--- api/src/routes/repos.ts
+++ api/src/routes/repos.ts
@@ -1,12 +1,23 @@
 import { Router, Request, Response } from 'express';
 
+import RepoServices from '../services/repos.service';
+
 export const repos = Router();
 
 repos.get('/', async (_: Request, res: Response) => {
   res.header('Cache-Control', 'no-store');
 
-  res.status(200);
+  try {
+    const [reposFromGitHub, reposFromJSON] = await Promise.all([
+      RepoServices.getListOfReposFromGitHub(),
+      RepoServices.getListOfReposFromJSON(),
+    ]);
 
-  // TODO: See README.md Task (A). Return repo data here. You’ve got this!
-  res.json([]);
+    res
+      .status(200)
+      .json([...reposFromGitHub, ...reposFromJSON])
+      .end();
+  } catch (error) {
+    res.status(500).json('Unable to fetch Data').end();
+  }
 });
---
+++ api/src/services/repos.service.ts
@@ -0,0 +1,45 @@
+
+import path from 'path'
+import fetch from 'node-fetch'
+import fs from 'fs'
+import util from 'util'
+
+const readFile = util.promisify(fs.readFile);
+
+const API_URL = `https://api.github.com/users/silverorange/repos`
+const REPOS_FILE_PATH =path.join(__dirname,'../..','/data/repos.json') 
+
+const getListOfReposFromGitHub = async () =>{
+
+    try{
+    // fetch the respos from github
+    const response = await fetch(API_URL);
+    const repos = await response.json();
+    return repos;
+    }
+    catch(error){
+        throw new Error("Unable to fetch Github Repos for silverorange")
+    }
+
+} 
+
+const getListOfReposFromJSON = async () =>{
+
+    try{
+    // fetch the respos from file json
+
+    const response = await readFile(REPOS_FILE_PATH)
+    const repos  = JSON.parse(response.toString())
+    return repos;
+
+    }
+    catch(error){
+        throw new Error("Unable to fetch Github Repos for JSON File")
+    }
+
+} 
+
+export default {
+    getListOfReposFromGitHub,
+    getListOfReposFromJSON
+};
