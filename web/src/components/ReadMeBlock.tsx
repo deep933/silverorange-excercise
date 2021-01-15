da80a8ee60ac1e565e9ca9b216a0026b372a4ce2
deepmal933 <deepmal944@gmail.com>
2021-01-15 12:10:57 -0500

    add dependency for readme markdown

--- web/src/components/ReadMeBlock.tsx
+++ web/src/components/ReadMeBlock.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import useApi from '../hooks/useApi';
+import ReactMarkdown from 'react-markdown';
 
 interface PropType {
   readMeURL: string | null;
 }
+
+/* Display ReadMe markdown by fetching it from provided readme url  */
 
 function ReadMeBlock({ readMeURL }: PropType) {
   const [isLoading, readMe, readMeError] = useApi({
@@ -15,7 +18,11 @@ function ReadMeBlock({ readMeURL }: PropType) {
     <div id="readMeBlock">
       <h3>ReadMe</h3>
       {isLoading ? <span className="loading">Loading ...</span> : <></>}
-      {!isLoading && readMe && <div className="box">{readMe}</div>}
+      {!isLoading && readMe && (
+        <div className="box">
+          <ReactMarkdown>{readMe}</ReactMarkdown>
+        </div>
+      )}
       {!isLoading && readMeError && (
         <span className="error">Unable to Fetch Commits</span>
       )}
